05573681206164ab9d85ed4d9cf1f742774ac2a3
Suriya <suriyaprakash.s666@gmail.com>
2023-03-25 12:36:19 +0530

    Files Added

--- English_Dictionary/index.js
+++ English_Dictionary/index.js
@@ -1,33 +1,41 @@
-const InputElement = document.getElementById('input');
-const NoteElement = document.getElementById('note');
-const meaningContainerElement = document.getElementById('meaning-container');
-const titleElement = document.getElementById('title');
-const meaningElement = document.getElementById('meaning');
+const InputElement = document.getElementById("input");
+const NoteElement = document.getElementById("note");
+const meaningContainerElement = document.getElementById("meaning-container");
+const titleElement = document.getElementById("title");
+const meaningElement = document.getElementById("meaning");
 const audioElement = document.getElementById("audio");
 
-async function fetchAPI(word){
-    try{
-        NoteElement.style.display = "block";
-        meaningContainerElement.style.display = "none";
-        NoteElement.innerText = `Searching the meaning of '${word}'`;
+async function fetchAPI(word) {
+  try {
+    NoteElement.style.display = "block";
+    meaningContainerElement.style.display = "none";
+    NoteElement.innerText = `Searching the meaning of '${word}'`;
 
-        const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-        const result = await fetch(url).then((res) => res.json());
-        console.log(result);
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    const result = await fetch(url).then((res) => res.json());
+    console.log(result);
 
-        NoteElement.style.display = "none";
-        meaningContainerElement.style.display = "block";
-        titleElement.innerText = result[0].word;
-        meaningElement.innerText = result[0].meanings[0].definitions[0].definition;
-        audioElement.src = result[0].phonetics[0].audio;   
-        
-    } catch(error) {
-        console.log(error);
+    if (result.title) {
+      titleElement.innerText = word;
+      meaningElement.innerText = "N/A";
+      audioElement.style.display = "none";
+    } else {
+      NoteElement.style.display = "none";
+      meaningContainerElement.style.display = "block";
+      audioElement.style.display = "inline-flex";
+      titleElement.innerText = result[0].word;
+      meaningElement.innerText =
+        result[0].meanings[0].definitions[0].definition;
+      audioElement.src = result[0].phonetics[0].audio;
     }
+  } catch (error) {
+    console.log(error);
+    NoteElement.innerText = `an error occurred!s try again later`;
+  }
 }
 
 InputElement.addEventListener("keyup", (e) => {
-    if(e.target.value && e.key === 'Enter'){
-        fetchAPI(e.target.value);
-    }
+  if (e.target.value && e.key === "Enter") {
+    fetchAPI(e.target.value);
+  }
 });
---
+++ snakeGame/index.js
@@ -0,0 +1,90 @@
+const playBoard = document.querySelector('.play-board');
+const ScoreElement = document.querySelector('.score');
+const HighScoreElement = document.querySelector('.high-score');
+const Controls = document.querySelectorAll('.controls i');
+
+let gameOver = false;
+let setIntervalId;
+let foodX, foodY;
+let snakeX = 10, snakeY = 16;
+let snakeBody = [];
+let velocityX = 0, velocityY = 0;
+let score = 0;
+
+
+let highScore = localStorage.getItem('high-score') || 0;
+HighScoreElement.innerText = `High Score : ${highScore}`;
+
+const ChangeFoodPosition = (e) => {
+
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+}
+
+const gameOverHandler = () => {
+    clearInterval(setIntervalId);
+    alert("Game Over! click Ok to restart...")
+    location.reload();
+}
+
+const changeDirection = (e) => {
+    if(e.key === "ArrowUp" && velocityY != 1 ){
+        velocityX = 0;
+        velocityY = -1; 
+    } else if(e.key === "ArrowDown" && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;   
+    } else if(e.key === "ArrowLeft" && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;   
+    } else if(e.key === "ArrowRight" && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;   
+    }
+}
+
+Controls.forEach(key => {
+    key.addEventListener("click", () => changeDirection({key: key.dataset.key}));
+});
+
+const GameInit = () => {
+    if(gameOver) return gameOverHandler();
+    let htmlMarkup = `<div class="food" style="grid-area:${foodY} / ${foodX}"></div>`;
+    
+    if(snakeX === foodX && snakeY === foodY){
+        ChangeFoodPosition();
+        snakeBody.push([foodX, foodY]);
+        score++;
+        highScore = score >= highScore  ? score : highScore;
+        localStorage.setItem('high-score', highScore);
+
+        ScoreElement.innerText = `Score : ${score}`;
+    }
+
+    for(let i = snakeBody.length - 1; i>0; i--){
+        snakeBody[i] = snakeBody[i-1];
+    }
+
+    snakeBody[0] = [snakeX, snakeY];
+
+    snakeX += velocityX;
+    snakeY += velocityY;
+
+    if(snakeX <=0 || snakeX >30 || snakeY <=0 || snakeY>30){
+        gameOver = true;
+    }
+
+
+    for(let i = 0; i< snakeBody.length; i++){
+        htmlMarkup += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+        if(i!==0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]){
+            gameOver = true;
+        }
+    }
+    playBoard.innerHTML = htmlMarkup;
+
+}
+
+ChangeFoodPosition();
+setIntervalId = setInterval(GameInit, 120);
+document.addEventListener("keydown", changeDirection);
